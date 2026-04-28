902f02864302e033be8ce142e49e4c1ebfc61242
Mahesh1-code141 <maheshbabuya@gmail.com>
2026-04-28 22:09:10 +0530

    Update user_repository.py

--- repositories/user_repository.py
+++ repositories/user_repository.py
@@ -52,7 +52,20 @@ def create_user(name, email, address, phonenumber, hashed_password):
 
     users = get_user_by_id(user_id)
     return users[0] if users else None
+    
+def get_all_users():
+    with closing(get_connection()) as db:
+        with closing(db.cursor()) as cursor:
+            select_query = """
+            SELECT id, name, email, Address, phonenumber
+            FROM user
+            ORDER BY id ASC
+            """
+            cursor.execute(select_query)
+            rows = cursor.fetchall()
 
+    return [row_to_dict(row) for row in rows]
+    
 
 def get_user_by_id(user_id):
     with closing(get_connection()) as db:
