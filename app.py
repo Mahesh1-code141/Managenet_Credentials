22b06b7c38937caa110a3aad52a18305740239d6
Mahesh1-code141 <maheshbabuya@gmail.com>
2026-04-28 23:27:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def get_data():
 
 @app.route('/show-all', methods=['GET', 'POST'])
 def show_all():
-    users = show_all_data()
+    users = show_all_data(user_id,name, email, address, phonenumber)
     return render_template("showall.html", users=users)
 
 
